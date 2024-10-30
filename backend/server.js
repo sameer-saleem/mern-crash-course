9e06e07f4d05ae724542cfd448e3785661ce05f1
sameer-saleem <saleemsameer736@gmail.com>
2024-10-30 14:53:22 -0700

    setting up dp

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 // const express = require('express');
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js' ;
+
+dotenv.config();
 
 const app = express(); 
 
-app.get("/products", (req, res)=>{
-    res.send('Server is ready 123');
-})
+app.get("/products", (req, res)=>{});
+
+console.log(process.env.MONGO_URI);
 
 app.listen(5000, ()=>{
+    connectDB();
     console.log('Server started at localhost:120000000');
-});
+}); 
